ce2f40c8256e9a704dde4e2761dd0a341b7ab5b9
Reza <Khodabakhshi65@yahoo.com>
2022-11-26 15:28:32 -0500

    firstClass

---
+++ Marshmallow/src/Hello/User.java
@@ -0,0 +1,41 @@
+package Hello;
+public class User {
+	
+	String userName; 
+	String password;
+	public String firstName;
+	public String lastName;
+	public String email;
+	String securityQuestion;
+	
+	public void register() {
+	}
+	public void logIn() {
+	}
+	public void logOut(){
+	}
+	public void setFirstName(){
+	}
+	public void setLastName() {
+	}
+	public void setAddress() {
+	}
+	public void setZip() {
+	}
+	public void setState() {
+	}
+	public void setEmail() {
+	}
+	public void setSecurityQuestion() {
+	}
+	public void goMainMenu() {
+	}
+	public void bookFlight() {
+	}
+	public void deleteFlight() {
+	}
+	public void retrievePassword() {
+	}
+	public void searchFlight() {
+	}
+}
