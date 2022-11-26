25246ea274aa8cd648c78e4690b76fb07c7f33d4
Reza <Khodabakhshi65@yahoo.com>
2022-11-26 15:39:58 -0500

    sixthClass

---
+++ Marshmallow/src/Hello/Admin.java
@@ -0,0 +1,13 @@
+package Hello;
+
+public class Admin extends User{
+	
+	public int adminId;
+	
+	public void addFlight() {
+	}
+	public void updateFlight() {
+	}
+	public void cancelFlight() {
+	}
+}
