e72aedd94d918c351c2ba4dc565de82b27337a68
Shaikhomama <omamashaikh50@gmail.com>
2022-11-27 18:46:42 -0500

    Merge branch 'master' of https://github.com/ellasimm/Marshmallow.git

--- Marshmallow/src/Hello/User.java
+++ Marshmallow/src/Hello/User.java
@@ -1,4 +1,12 @@
 package Hello;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class User {
 	
 	private String userName; 
@@ -8,7 +16,61 @@ public class User {
 	private String email;
 	private String securityQuestion;
 	
-	public void register() {
+	public void register() throws FileNotFoundException{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter username: ");
+		this.userName = input.nextLine();
+		System.out.println("Enter password: ");
+		this.password = input.nextLine();
+		System.out.println("Confirm Password: ");
+		String confirmPassword = input.nextLine();
+		
+		String str = userName + " " + confirmPassword;
+		if (password.equals(confirmPassword)) {
+			File f = new File("User.txt");
+			Scanner scan = new Scanner(f);
+			int flag = 0;
+			while(scan.hasNextLine()) {
+				String data = scan.nextLine();
+				if(data.equals(str)) {
+					System.out.println("Already Registered.");
+					flag = 1;
+					System.out.println("1. Registeration");
+					System.out.println("2. LogIn");
+					
+					System.out.println("Enter your choice: ");
+					int choice = input.nextInt();
+					if(choice == 1) {
+						this.register();
+					}
+					else if (choice == 2){
+						this.logIn();
+					}
+					else {
+						System.out.println("Enter proper choice.");
+					}
+					break;
+				}
+				scan.close();
+				
+				if (flag == 0) {
+					try {
+						BufferedWriter out = new BufferedWriter(new FileWriter("User.txt", true));
+						out.write(userName +" "+ password+ "\n");
+						out.close();
+					}catch(IOException ex) {
+						System.out.println("Exception happened" + ex);
+					}
+					System.out.println("Successfully Registered.");
+					System.out.println("Please Login");
+					this.logIn;
+				}
+				
+			}
+			System.out.println("Password is verified.");
+			
+		}
+		
 	}
 	public void logIn() {
 	}
