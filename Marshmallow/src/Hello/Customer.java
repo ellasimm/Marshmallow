48e1c2045789c925ca15bbc1790b0513fc9d2564
Reza <Khodabakhshi65@yahoo.com>
2022-11-27 22:01:47 -0500

    11/27

--- Marshmallow/src/Hello/Customer.java
+++ Marshmallow/src/Hello/Customer.java
@@ -2,8 +2,31 @@ package Hello;
 
 public class Customer extends User{
 	
+	private User userName; 
+	private User password;
+	private User firstName;
+	private User lastName;
+	private User email;
+	private User securityQuestion;
 	private int passengerId;
 	private String address;
 	private int zip;
 	private String state;
+	
+	public Customer() {
+	}
+	public Customer(User userName, User password, User firstName, User lastName, User email,
+			        User securityQuestion, int passengerId, String address, int zip, String state) {
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.securityQuestion = securityQuestion;
+		this.passengerId = passengerId;
+		this.address = address;
+		this.zip = zip;
+		this.state = state;
+	}
+	
 }
