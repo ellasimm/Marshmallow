ab26fba4b989a98ee290c1a5fa8291e0a48f5e0d
Shaikhomama <omamashaikh50@gmail.com>
2022-12-01 18:49:36 -0500

    O.S(12/1)

--- Marshmallow/src/Database/SearchDB.java
+++ Marshmallow/src/Database/SearchDB.java
@@ -1,5 +1,14 @@
 package Database;
 
+import FlightCode.Flights;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class SearchDB {
+	public static ObservableList<Flights> searchFlight(){
+		ObservableList<Flights> flights = FXCollections.observableArrayList();
+		//add more
+		return flights;
+	}
 
 }
--- Marshmallow/src/Database/UpdateDB.java
+++ Marshmallow/src/Database/UpdateDB.java
@@ -1,5 +1,14 @@
 package Database;
 
 public class UpdateDB {
+	public static void deleteFlightOrder(int orderNumber) {
+		
+	}
+	public static void deleteFlight(int flightId) {
+		
+	}
+	public static void addFlight(int flightId) {
+		
+	}
 
 }
