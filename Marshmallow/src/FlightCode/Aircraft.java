92c389096d9c21963775bae7b2f4ced748941eaf
ellasimm <ella.josephine.simm@gmail.com>
2022-12-01 16:24:12 -0500

    Deleted unneeded class

--- Marshmallow/src/FlightCode/Aircraft.java
+++
@@ -1,9 +0,0 @@
-package FlightCode;
-public class Aircraft {
-	
-	private String aircraftMake;
-	private String aircraftModel;
-	private int aircraftId;
-	private String airlineName;
-	private int aircraftCapacity;
-}
