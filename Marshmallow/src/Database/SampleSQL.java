c17e12399c175d8f8a3ea4c0d18ca0e09fc99c87
ellasimm <ella.josephine.simm@gmail.com>
2022-12-05 12:29:05 -0500

    data

--- Marshmallow/src/Database/SampleSQL.java
+++ Marshmallow/src/Database/SampleSQL.java
@@ -27,7 +27,7 @@ public class SampleSQL {
 			resultSet = statement.executeQuery(sql);
 			while(resultSet.next()) {
 				System.out.println(resultSet.getString(1) + ", "+ resultSet.getString(2)+", "+ resultSet.getString(3)
-									+ ", " + resultSet.getString(4) + ", "+ resultSet.getString(5)+", "+ resultSet.getString(6));	
+									+ ", " + resultSet.getString(4) + ", "+ resultSet.getString(5)+", "+ resultSet.getString(6) +", "+ resultSet.getString(7));	
 			}
 		}	
 		catch (SQLException e) {
